f09c5811ef2195de4c632e74dc0d269fab5d2ac2
Michael Crawford <mbcrawfo@gmail.com>
2022-05-12 00:35:14 -0400

    diff_commonSuffix -> StringExtensions.FindCommonSuffix

--- source/SharpDmp/Extensions/StringExtensions.cs
+++ source/SharpDmp/Extensions/StringExtensions.cs
@@ -10,6 +10,11 @@ public static class StringExtensions
     /// <returns>
     ///     The number of characters in common at the start of each string.
     /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="text1"/> is null.
+    ///     -or-
+    ///     <paramref name="text2"/> is null.
+    /// </exception>
     public static int FindCommonPrefix(this string text1, string text2)
     {
         if (text1 is null)
@@ -29,6 +34,44 @@ public static class StringExtensions
             if (text1[i] != text2[i])
             {
                 return i;
+            }
+        }
+
+        return lengthToCheck;
+    }
+
+    /// <summary>
+    ///     Determines the common suffix of two strings.
+    /// </summary>
+    /// <param name="text1"></param>
+    /// <param name="text2"></param>
+    /// <returns>
+    ///     The number of characters in common at the end of each string.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="text1"/> is null.
+    ///     -or-
+    ///     <paramref name="text2"/> is null.
+    /// </exception>
+    public static int FindCommonSuffix(this string text1, string text2)
+    {
+        if (text1 is null)
+        {
+            throw new ArgumentNullException(nameof(text1));
+        }
+
+        if (text2 is null)
+        {
+            throw new ArgumentNullException(nameof(text2));
+        }
+
+        var lengthToCheck = Math.Min(text1.Length, text2.Length);
+
+        for (var i = 1; i <= lengthToCheck; i++)
+        {
+            if (text1[^i] != text2[^i])
+            {
+                return i - 1;
             }
         }
 
--- tests/SharpDmp.UnitTests/Extensions/StringExtensionsTests.cs
+++ tests/SharpDmp.UnitTests/Extensions/StringExtensionsTests.cs
@@ -61,4 +61,59 @@ public class StringExtensionsTests
         // assert
         actual.Should().Be(expected);
     }
+
+    [Fact]
+    public void FindCommonSuffix_ShouldThrowArgumentNullException_WhenText1IsNull()
+    {
+        // arrange
+        // act
+        Action act = () => _ = StringExtensions.FindCommonSuffix(null!, "");
+
+        // assert
+        act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("text1");
+    }
+
+    [Fact]
+    public void FindCommonSuffix_ShouldThrowArgumentNullException_WhenText2IsNull()
+    {
+        // arrange
+        // act
+        Action act = () => _ = "".FindCommonSuffix(null!);
+
+        // assert
+        act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("text2");
+    }
+
+    [Theory]
+    [InlineData("abc", "xyz")]
+    [InlineData("abcdef", "xyz")]
+    [InlineData("abc", "uvwxyz")]
+    public void FindCommonSuffix_ShouldReturnZero_WhenInputsHaveNoCommonSuffix(string text1, string text2)
+    {
+        // arrange
+        // act
+        var actual = text1.FindCommonSuffix(text2);
+
+        // assert
+        actual.Should().Be(0);
+    }
+
+    [Theory]
+    [InlineData("acdef1234", "xyz1234", 4)]
+    [InlineData("xyz1234", "acdef1234", 4)]
+    [InlineData("1234", "xyz1234", 4)]
+    [InlineData("xyz1234", "1234", 4)]
+    public void FindCommonSuffix_ShouldReturnLengthOfSuffix_WhenInputsHaveCommonSuffix(
+        string text1,
+        string text2,
+        int expected
+    )
+    {
+        // arrange
+        // act
+        var actual = text1.FindCommonSuffix(text2);
+
+        // assert
+        actual.Should().Be(expected);
+    }
 }
