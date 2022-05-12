39459ce709485ebb5b900ebf4ecf77d346c5f9a5
Michael Crawford <mbcrawfo@gmail.com>
2022-05-12 01:17:54 -0400

    diff_commonOverlap -> ReadOnlySpanExtensions.FindCommonOverlap

--- source/SharpDmp/Extensions/ReadOnlySpanExtensions.cs
+++ source/SharpDmp/Extensions/ReadOnlySpanExtensions.cs
@@ -47,4 +47,54 @@ public static class ReadOnlySpanExtensions
 
         return lengthToCheck;
     }
+
+    /// <summary>
+    ///     Determines if the suffix of the first string is the prefix of the second string.
+    /// </summary>
+    /// <param name="text1"></param>
+    /// <param name="text2"></param>
+    /// <returns>
+    ///     The number of characters in common at the end of the first string and the start of the second string.
+    /// </returns>
+    public static int FindCommonOverlap(this ReadOnlySpan<char> text1, ReadOnlySpan<char> text2)
+    {
+        if (text1.Length is 0 || text2.Length is 0)
+        {
+            return 0;
+        }
+
+        var truncatedText1 = text1.Length > text2.Length ? text1[^text2.Length..] : text1;
+        var truncatedText2 = text1.Length < text2.Length ? text2[..text1.Length] : text2;
+
+        var lengthToCheck = Math.Min(text1.Length, text2.Length);
+        if (truncatedText1.Equals(truncatedText2, StringComparison.Ordinal))
+        {
+            return lengthToCheck;
+        }
+
+        var best = 0;
+        var length = 1;
+        while (true)
+        {
+            var pattern = truncatedText1[(lengthToCheck - length)..];
+            var found = truncatedText2.IndexOf(pattern, StringComparison.Ordinal);
+            if (found == -1)
+            {
+                return best;
+            }
+
+            length += found;
+            if (
+                found is 0
+                || truncatedText1[(lengthToCheck - length)..].Equals(
+                    truncatedText2[..lengthToCheck],
+                    StringComparison.Ordinal
+                )
+            )
+            {
+                best = length;
+                length += 1;
+            }
+        }
+    }
 }
--- tests/SharpDmp.UnitTests/Extensions/ReadOnlySpanExtensionsTests.cs
+++ tests/SharpDmp.UnitTests/Extensions/ReadOnlySpanExtensionsTests.cs
@@ -72,4 +72,34 @@ public class ReadOnlySpanExtensionsTests
         // assert
         actual.Should().Be(expected);
     }
+
+    [Theory]
+    [InlineData("", "abcd")]
+    [InlineData("abcd", "")]
+    public void FindCommonOverlap_ShouldReturnZero_WhenOneInputIsEmpty(string text1, string text2)
+    {
+        // arrange
+        // act
+        var actual = text1.AsSpan().FindCommonOverlap(text2);
+
+        // assert
+        actual.Should().Be(0);
+    }
+
+    [Theory]
+    [InlineData("123456", "abcd", 0)]
+    [InlineData("abcd", "123456", 0)]
+    [InlineData("fi", "\ufb01i", 0)]
+    [InlineData("\ufb01i", "fi", 0)]
+    [InlineData("abc", "abcd", 3)]
+    [InlineData("123456xxx", "xxxabcd", 3)]
+    public void FindCommonOverlap_ShouldReturnNumberOfCommonCharacters(string text1, string text2, int expected)
+    {
+        // arrange
+        // act
+        var actual = text1.AsSpan().FindCommonOverlap(text2);
+
+        // assert
+        actual.Should().Be(expected);
+    }
 }
