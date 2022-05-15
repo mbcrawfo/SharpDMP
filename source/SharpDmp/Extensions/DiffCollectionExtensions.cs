7745d628e032ada875c67a8a49bc1550dfdd121b
Michael Crawford <mbcrawfo@gmail.com>
2022-05-15 18:36:28 -0400

    diff_cleanupMerge -> DiffCollectionExtensions.CleanupAndMerge

---
+++ source/SharpDmp/Extensions/DiffCollectionExtensions.cs
@@ -0,0 +1,267 @@
+﻿using System.Diagnostics;
+using System.Text;
+using SharpDmp.Data;
+
+namespace SharpDmp.Extensions;
+
+public static class DiffCollectionExtensions
+{
+    /// <summary>
+    ///     Reorder and merge together redundant edit sections.  Multiple equalities are merged together.  Any other
+    ///     edit section can move as long as it doesn't cross an equality.
+    /// </summary>
+    /// <param name="diffs">
+    ///     The diffs to be optimized.
+    /// </param>
+    /// <returns>
+    ///     A new collection of diffs that have been optimized.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="diffs"/> is null.
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     One of the items in <paramref name="diffs"/> contains an invalid <see cref="Operation"/> value.
+    /// </exception>
+    public static IReadOnlyList<Diff> CleanupAndMerge(this IEnumerable<Diff> diffs)
+    {
+        if (diffs is null)
+        {
+            throw new ArgumentNullException(nameof(diffs));
+        }
+
+        // Copy into a list that we can mutate.
+        var resultDiffs = diffs.ToList();
+        var dummyDiff = new Diff(Operation.Equal, string.Empty);
+
+        while (true)
+        {
+            // Add a dummy equality at the end to trigger merging any trailing inserts and deletes.
+            resultDiffs.Add(dummyDiff);
+
+            var index = 0;
+            var numberOfDeletions = 0;
+            var textDeleted = new StringBuilder();
+            var numberOfInsertions = 0;
+            var textInserted = new StringBuilder();
+
+            while (index < resultDiffs.Count)
+            {
+                var currentDiff = resultDiffs[index];
+                switch (currentDiff.Operation)
+                {
+                    case Operation.Delete:
+                        numberOfDeletions += 1;
+                        textDeleted.Append(currentDiff.Text);
+                        index += 1;
+                        break;
+
+                    case Operation.Insert:
+                        numberOfInsertions += 1;
+                        textInserted.Append(currentDiff.Text);
+                        index += 1;
+                        break;
+
+                    case Operation.Equal:
+                        if (numberOfDeletions + numberOfInsertions > 1)
+                        {
+                            index = MergeInsertAndDeleteCommonalities(
+                                resultDiffs,
+                                index,
+                                numberOfDeletions,
+                                textDeleted,
+                                numberOfInsertions,
+                                textInserted
+                            );
+                        }
+                        // Merge back to back equalities together.
+                        else if (index is not 0 && resultDiffs[index - 1].Operation is Operation.Equal)
+                        {
+                            var previousDiff = resultDiffs[index - 1];
+                            resultDiffs[index - 1] = previousDiff with { Text = previousDiff.Text + currentDiff.Text };
+                            resultDiffs.RemoveAt(index);
+                        }
+                        // Remove empty equalities, but ignore the dummy diff at the end.
+                        else if (index < resultDiffs.Count - 1 && currentDiff.Text.Length == 0)
+                        {
+                            resultDiffs.RemoveAt(index);
+                        }
+                        else
+                        {
+                            index += 1;
+                        }
+
+                        numberOfDeletions = 0;
+                        textDeleted.Clear();
+                        numberOfInsertions = 0;
+                        textInserted.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException(
+                            nameof(diffs),
+                            currentDiff.Operation,
+                            $"{nameof(Diff)} contains an invalid {nameof(Operation)} value."
+                        );
+                }
+            }
+
+            // Remove the dummy entry, if it's still there.
+            if (resultDiffs[^1] is { Operation: Operation.Equal, Text: "" })
+            {
+                resultDiffs.RemoveAt(resultDiffs.Count - 1);
+            }
+
+            // If any shifts were made we need another pass at reordering.  If not, we're done.
+            if (!OptimizeSingleEdits(resultDiffs))
+            {
+                return resultDiffs.AsReadOnly();
+            }
+        }
+    }
+
+    /// <summary>
+    ///     When the current diff is an equality preceeded by insertions or deletions, see if we can merge together
+    ///     any common prefixes or suffixes from the previous diffs.
+    /// </summary>
+    /// <param name="diffs"></param>
+    /// <param name="index"></param>
+    /// <param name="numberOfDeletions"></param>
+    /// <param name="textDeleted"></param>
+    /// <param name="numberOfInsertions"></param>
+    /// <param name="textInserted"></param>
+    /// <returns></returns>
+    private static int MergeInsertAndDeleteCommonalities(
+        List<Diff> diffs,
+        int index,
+        int numberOfDeletions,
+        StringBuilder textDeleted,
+        int numberOfInsertions,
+        StringBuilder textInserted
+    )
+    {
+        Debug.Assert(diffs[index].Operation == Operation.Equal, "diffs[index].Operation == Operation.Equal");
+
+        // Cache each string as they're allocated off of the StringBuilders so that we can avoid duplicate
+        // allocations unless the StringBuilder contents were actually changed.
+        var deletedBeforePrefix = textDeleted.ToString();
+        var insertedBeforePrefix = textInserted.ToString();
+        var prefixLength = 0;
+        var deletedBeforeSuffix = string.Empty;
+        var insertedBeforeSuffix = string.Empty;
+        var suffixLength = 0;
+
+        if (numberOfDeletions is not 0 && numberOfInsertions is not 0)
+        {
+            // Factor out any common prefixes.
+            prefixLength = insertedBeforePrefix.AsSpan().FindCommonPrefix(deletedBeforePrefix);
+            if (prefixLength is not 0)
+            {
+                var commonPrefix = insertedBeforePrefix[..prefixLength];
+                var candidateEqualityIndex = Math.Max(0, index - numberOfDeletions - numberOfInsertions - 1);
+                if (diffs[candidateEqualityIndex].Operation is Operation.Equal)
+                {
+                    var diffToUpdate = diffs[candidateEqualityIndex];
+                    diffs[candidateEqualityIndex] = diffToUpdate with { Text = diffToUpdate.Text + commonPrefix };
+                }
+                else
+                {
+                    diffs.Insert(0, new Diff(Operation.Equal, commonPrefix));
+                    index += 1;
+                }
+
+                textDeleted.Remove(0, prefixLength);
+                textInserted.Remove(0, prefixLength);
+            }
+
+            // Factor out any common suffixes.
+            deletedBeforeSuffix = prefixLength is 0 ? deletedBeforePrefix : textDeleted.ToString();
+            insertedBeforeSuffix = prefixLength is 0 ? insertedBeforePrefix : textInserted.ToString();
+            suffixLength = insertedBeforeSuffix.AsSpan().FindCommonSuffix(deletedBeforeSuffix);
+            if (suffixLength is not 0)
+            {
+                var commonSuffix = insertedBeforeSuffix[(textInserted.Length - suffixLength)..];
+                diffs[index] = diffs[index] with { Text = commonSuffix + diffs[index].Text };
+
+                textDeleted.Remove(textDeleted.Length - suffixLength, suffixLength);
+                textInserted.Remove(textInserted.Length - suffixLength, suffixLength);
+            }
+        }
+
+        // Remove the diffs we've been merging, and reinsert any remaining deletes or inserts
+        index -= numberOfDeletions + numberOfInsertions;
+        diffs.RemoveRange(index, numberOfDeletions + numberOfInsertions);
+
+        if (textDeleted.Length is not 0)
+        {
+            var newDeleted = (prefixLength, suffixLength) switch
+            {
+                (0, 0) => deletedBeforePrefix,
+                (> 0, 0) => deletedBeforeSuffix,
+                _ => textDeleted.ToString()
+            };
+            diffs.Insert(index, new Diff(Operation.Delete, newDeleted));
+            index += 1;
+        }
+
+        if (textInserted.Length is not 0)
+        {
+            var newInserted = (prefixLength, suffixLength) switch
+            {
+                (0, 0) => insertedBeforePrefix,
+                (> 0, 0) => insertedBeforeSuffix,
+                _ => textInserted.ToString()
+            };
+            diffs.Insert(index, new Diff(Operation.Insert, newInserted));
+            index += 1;
+        }
+
+        return index + 1;
+    }
+
+    /// <summary>
+    ///     Look for single edits surrounded on both sides by equalities which can be shifted left or right to
+    ///     eliminate one of the equalities, e.g. A<ins>BA</ins>C -> <ins>AB</ins>AC
+    /// </summary>
+    /// <param name="diffs"></param>
+    /// <returns>
+    ///     True if any changes were made to <paramref name="diffs"/>.
+    /// </returns>
+    private static bool OptimizeSingleEdits(IList<Diff> diffs)
+    {
+        var changes = false;
+
+        // Intentionally ignore the first and last element (they don't qualifity).
+        var index = 1;
+        while (index < diffs.Count - 1)
+        {
+            var previousDiff = diffs[index - 1];
+            var currentDiff = diffs[index];
+            var nextDiff = diffs[index + 1];
+
+            if (previousDiff.Operation is Operation.Equal && nextDiff.Operation is Operation.Equal)
+            {
+                if (currentDiff.Text.EndsWith(previousDiff.Text, StringComparison.Ordinal))
+                {
+                    diffs[index] = currentDiff with
+                    {
+                        Text = previousDiff.Text + currentDiff.Text[..^previousDiff.Text.Length]
+                    };
+                    diffs[index + 1] = nextDiff with { Text = previousDiff.Text + nextDiff.Text };
+                    diffs.RemoveAt(index - 1);
+                    changes = true;
+                }
+                else if (currentDiff.Text.StartsWith(nextDiff.Text, StringComparison.Ordinal))
+                {
+                    diffs[index - 1] = previousDiff with { Text = previousDiff.Text + nextDiff.Text };
+                    diffs[index] = currentDiff with { Text = currentDiff.Text[nextDiff.Text.Length..] + nextDiff.Text };
+                    diffs.RemoveAt(index + 1);
+                    changes = true;
+                }
+            }
+
+            index += 1;
+        }
+
+        return changes;
+    }
+}
---
+++ tests/SharpDmp.UnitTests/Extensions/DiffCollectionExtensionsTests.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using FluentAssertions;
+using SharpDmp.Data;
+using SharpDmp.Extensions;
+using Xunit;
+
+namespace SharpDmp.UnitTests.Extensions;
+
+public class DiffCollectionExtensionsTests
+{
+    [Theory]
+    [InlineData(Operation.Delete, Operation.Insert)]
+    [InlineData(Operation.Insert, Operation.Delete)]
+    public void CleanupAndMerge_ShouldOptimizeCommonPrefixesForDeletionsAndInserts(
+        Operation operationToRemove,
+        Operation operationToRemain
+    )
+    {
+        // arrange
+        var diffs = new[] { new Diff(operationToRemove, "a"), new Diff(operationToRemain, "abc"), };
+
+        var expected = new[] { new Diff(Operation.Equal, "a"), new Diff(operationToRemain, "bc"), };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Slide edit left
+    [Theory]
+    [InlineData(Operation.Delete)]
+    [InlineData(Operation.Insert)]
+    public void CleanupAndMerge_ShouldMergeEqualities_WhenSeparatedByAnOperationThatEndsWithTheFirstEquality(
+        Operation operation
+    )
+    {
+        // arrange
+        var diffs = new[] { new Diff(Operation.Equal, "a"), new Diff(operation, "ba"), new Diff(Operation.Equal, "c") };
+
+        var expected = new[] { new Diff(operation, "ab"), new Diff(Operation.Equal, "ac") };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Slide edit right
+    [Theory]
+    [InlineData(Operation.Delete)]
+    [InlineData(Operation.Insert)]
+    public void CleanupAndMerge_ShouldMergeEqualities_WhenSeparatedByAnOperationThatStartsWithTheTrailingEquality(
+        Operation operation
+    )
+    {
+        // arrange
+        var diffs = new[] { new Diff(Operation.Equal, "c"), new Diff(operation, "ab"), new Diff(Operation.Equal, "a") };
+
+        var expected = new[] { new Diff(Operation.Equal, "ca"), new Diff(operation, "ba") };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Slide edit left recursive
+    [Theory]
+    [InlineData(Operation.Delete)]
+    [InlineData(Operation.Insert)]
+    public void CleanupAndMerge_ShouldMergeEqualitiesAfterAnOperation_WhenMultiplePassesAreRequiredToFullyOptimize(
+        Operation operation
+    )
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Equal, "a"),
+            new Diff(operation, "b"),
+            new Diff(Operation.Equal, "c"),
+            new Diff(operation, "ac"),
+            new Diff(Operation.Equal, "x")
+        };
+
+        var expected = new[] { new Diff(operation, "abc"), new Diff(Operation.Equal, "acx") };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Slide edit right recursive
+    [Theory]
+    [InlineData(Operation.Delete)]
+    [InlineData(Operation.Insert)]
+    public void CleanupAndMerge_ShouldMergeEqualitiesBeforeAnOperation_WhenMultiplePassesAreRequiredToFullyOptimize(
+        Operation operation
+    )
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Equal, "x"),
+            new Diff(operation, "ca"),
+            new Diff(Operation.Equal, "c"),
+            new Diff(operation, "b"),
+            new Diff(Operation.Equal, "a")
+        };
+
+        var expected = new[] { new Diff(Operation.Equal, "xca"), new Diff(operation, "cba") };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Merge interweave
+    [Fact]
+    public void CleanupAndMerge_ShouldMergeOperationsTogether_WhenOperationsAreInterwoven()
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Delete, "a"),
+            new Diff(Operation.Insert, "b"),
+            new Diff(Operation.Delete, "c"),
+            new Diff(Operation.Insert, "d"),
+            new Diff(Operation.Equal, "e"),
+            new Diff(Operation.Equal, "f")
+        };
+
+        var expected = new[]
+        {
+            new Diff(Operation.Delete, "ac"),
+            new Diff(Operation.Insert, "bd"),
+            new Diff(Operation.Equal, "ef")
+        };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Merge deletions
+    [Fact]
+    public void CleanupAndMerge_ShouldMergeSequentialOperationsTogether_WhenOperationsAreDelete()
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Delete, "a"),
+            new Diff(Operation.Delete, "b"),
+            new Diff(Operation.Delete, "c")
+        };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(1).And.Contain(new Diff(Operation.Delete, "abc"));
+    }
+
+    // diff_cleanupMerge: Merge equalities
+    [Fact]
+    public void CleanupAndMerge_ShouldMergeSequentialOperationsTogether_WhenOperationsAreEqual()
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Equal, "a"),
+            new Diff(Operation.Equal, "b"),
+            new Diff(Operation.Equal, "c")
+        };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(1).And.Contain(new Diff(Operation.Equal, "abc"));
+    }
+
+    // diff_cleanupMerge: Merge insertions
+    [Fact]
+    public void CleanupAndMerge_ShouldMergeSequentialOperationsTogether_WhenOperationsAreInsert()
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Insert, "a"),
+            new Diff(Operation.Insert, "b"),
+            new Diff(Operation.Insert, "c")
+        };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(1).And.Contain(new Diff(Operation.Insert, "abc"));
+    }
+
+    // diff_cleanupMerge: No change case
+    [Fact]
+    public void CleanupAndMerge_ShouldNotChangeTheDiffs_WhenNoCleanupIsPossible()
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Equal, "a"),
+            new Diff(Operation.Delete, "b"),
+            new Diff(Operation.Insert, "c")
+        };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(diffs.Length).And.ContainInOrder(diffs);
+    }
+
+    // diff_cleanupMerge: Prefix and suffix detection
+    [Fact]
+    public void CleanupAndMerge_ShouldOptimizeCommonPrefixesAndSuffixesForDeletionsAndInserts()
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Delete, "a"),
+            new Diff(Operation.Insert, "abc"),
+            new Diff(Operation.Delete, "dc")
+        };
+
+        var expected = new[]
+        {
+            new Diff(Operation.Equal, "a"),
+            new Diff(Operation.Delete, "d"),
+            new Diff(Operation.Insert, "b"),
+            new Diff(Operation.Equal, "c")
+        };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Prefix and suffix detection with equalities
+    [Fact]
+    public void CleanupAndMerge_ShouldOptimizeCommonPrefixesAndSuffixesForDeletionsAndInserts_WhenEqualitiesAreIncluded()
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Equal, "x"),
+            new Diff(Operation.Delete, "a"),
+            new Diff(Operation.Insert, "abc"),
+            new Diff(Operation.Delete, "dc"),
+            new Diff(Operation.Equal, "y")
+        };
+
+        var expected = new[]
+        {
+            new Diff(Operation.Equal, "xa"),
+            new Diff(Operation.Delete, "d"),
+            new Diff(Operation.Insert, "b"),
+            new Diff(Operation.Equal, "cy")
+        };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    [Fact]
+    public void CleanupAndMerge_ShouldOptimizeCommonSuffixesForDeletionsAndInserts()
+    {
+        // arrange
+        var diffs = new[] { new Diff(Operation.Insert, "abc"), new Diff(Operation.Delete, "dc") };
+
+        var expected = new[]
+        {
+            new Diff(Operation.Delete, "d"),
+            new Diff(Operation.Insert, "ab"),
+            new Diff(Operation.Equal, "c")
+        };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Empty equality
+    [Fact]
+    public void CleanupAndMerge_ShouldRemoveEmptyEqualities()
+    {
+        // arrange
+        var diffs = new[] { new Diff(Operation.Equal, ""), new Diff(Operation.Insert, "a"), };
+
+        var expected = new[] { new Diff(Operation.Insert, "a") };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Empty merge
+    [Fact]
+    public void CleanupAndMerge_ShouldRemoveOperationsThatAreRedundant()
+    {
+        // arrange
+        var diffs = new[]
+        {
+            new Diff(Operation.Delete, "b"),
+            new Diff(Operation.Insert, "ab"),
+            new Diff(Operation.Equal, "c")
+        };
+
+        var expected = new[] { new Diff(Operation.Insert, "a"), new Diff(Operation.Equal, "bc") };
+
+        // act
+        var result = diffs.CleanupAndMerge();
+
+        // assert
+        result.Should().HaveCount(expected.Length).And.ContainInOrder(expected);
+    }
+
+    // diff_cleanupMerge: Null case.
+    [Fact]
+    public void CleanupAndMerge_ShouldReturnEmptyList_WhenInputIsEmpty()
+    {
+        // arrange
+        // act
+        var result = Array.Empty<Diff>().CleanupAndMerge();
+
+        // assert
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void CleanupAndMerge_ShouldThrowArgumentNullException_WhenDiffsIsNull()
+    {
+        // arrange
+        // act
+        Action act = () => _ = DiffCollectionExtensions.CleanupAndMerge(null!);
+
+        // assert
+        act.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("diffs");
+    }
+
+    [Fact]
+    public void CleanupAndMerge_ShouldThrowArgumentOutOfRangeException_WhenADiffHasAnUnknownOperation()
+    {
+        // arrange
+        var operation = (Operation)Enum.ToObject(typeof(Operation), -1);
+        var diffs = new[] { new Diff(operation, "abc") };
+
+        // act
+        Action act = () => _ = diffs.CleanupAndMerge();
+
+        // assert
+        act.Should()
+            .Throw<ArgumentOutOfRangeException>()
+            .WithMessage($"*invalid {nameof(Operation)}*")
+            .And.ParamName.Should()
+            .Be("diffs");
+    }
+}
